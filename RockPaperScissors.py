95c10651079a6b683007325040accc79760b8dbe
Domingas Pleckauskas <domingas.p@yahoo.com>
2019-10-09 21:11:03 +0100

    Updated GUI layout of player/computer choices. Added a delay to make the game more human and create anticipation towards the result.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,71 +1,4 @@
-<<<<<<< HEAD
-import random
-import tkinter as tk
-
-class MainGUI:
-    def __init__(self, master):
-        self.master = master
-        
-        self.mainFrame = tk.Frame(self.master)
-        self.mainFrame.grid()
-
-        self.playerScore = tk.Label(self.master, text = "P 0/3")
-        self.playerScore.grid(row = 1, column = 0)
-
-        self.computerScore = tk.Label(self.master, text = "C 0/3")
-        self.computerScore.grid(row = 1, column = 2)
-
-        self.playerChoices = tk.Label(self.master, text = "Please choose your weapon.")
-        self.playerChoices.grid(row = 2, columnspan = 3)
-
-        self.resultLabel = tk.Label(self.master, text = "Not started.")
-        self.resultLabel.grid(row = 3, columnspan = 3)
-
-        self.rockButton = tk.Button(self.master, text = "Rock", width = 10, height = 2, command = lambda: playGame(self, 0))
-        self.rockButton.grid(row = 4, column = 0)
-
-        self.paperButton = tk.Button(self.master, text = "Paper", width = 10, height = 2, command = lambda: playGame(self, 1))
-        self.paperButton.grid(row = 4, column = 1)
-
-        self.scissorsButton = tk.Button(self.master, text = "Scissors", width = 10, height = 2, command = lambda: playGame(self, 2))
-        self.scissorsButton.grid(row = 4, column = 2)
-
-        
-
-def playGame(gui, usersGo):
-    computersGo = random.randint(0, 2)
-
-    weapons = ["Rock", "Paper", "Scissors"]
-
-    gui.playerChoices["text"] = str(weapons[usersGo]) + " v " + str(weapons[computersGo])
-
-    if (usersGo == computersGo):
-        gui.resultLabel["text"] = "A Tie"
-    else:
-        if (usersGo == 0):
-            if (computersGo == 2):
-                gui.resultLabel["text"] = "You Won"
-            else:
-                gui.resultLabel["text"] = "You Lost"
-        elif (usersGo == 1):
-            if (computersGo == 0):
-                gui.resultLabel["text"] = "You Won"
-            else:
-                gui.resultLabel["text"] = "You Lost"
-        if (usersGo == 2):
-            if (computersGo == 1):
-                gui.resultLabel["text"] = "You Won"
-            else:
-                gui.resultLabel["text"] = "You Lost"
-
-def main():
-    root = tk.Tk()
-    app = MainGUI(root)
-    root.mainloop()
-
-if __name__ == "__main__":
-=======
-import random
+import random, time
 import tkinter as tk
 
 class MainGUI:
@@ -81,8 +14,14 @@ class MainGUI:
         self.computerScore = tk.Label(self.master, text = "C 0")
         self.computerScore.grid(row = 1, column = 2)
 
-        self.playerChoices = tk.Label(self.master, text = "Please choose your weapon.")
-        self.playerChoices.grid(row = 2, columnspan = 3)
+        self.playerChoice = tk.Label(self.master, text = "")
+        self.playerChoice.grid(row = 2, column = 0)
+
+        self.vsLabel = tk.Label(self.master, text = "Please choose your weapon.")
+        self.vsLabel.grid(row = 2, columnspan = 3)
+
+        self.computerChoice = tk.Label(self.master, text = "")
+        self.computerChoice.grid(row = 2, column = 2)
 
         self.resultLabel = tk.Label(self.master, text = "Not started.")
         self.resultLabel.grid(row = 3, columnspan = 3)
@@ -102,11 +41,30 @@ class GameStats:
         self.computerWon = 0
 
 def playGame(gui, usersGo, stats):
+    gui.rockButton["state"] = "disabled"
+    gui.paperButton["state"] = "disabled"
+    gui.scissorsButton["state"] = "disabled"
+
     computersGo = random.randint(0, 2)
 
     weapons = ["Rock", "Paper", "Scissors"]
 
-    gui.playerChoices["text"] = str(weapons[usersGo]) + " v " + str(weapons[computersGo])
+    gui.vsLabel["text"] = ""
+    gui.vsLabel.grid(row = 2, column = 1, columnspan = 1)
+
+    gui.playerChoice["text"] = "Rock"
+    gui.computerChoice["text"] = "Rock"
+    gui.master.update()
+    time.sleep(0.6)
+
+    gui.playerChoice["text"] = "Paper"
+    gui.computerChoice["text"] = "Paper"
+    gui.master.update()
+    time.sleep(0.6)
+
+    gui.vsLabel["text"] = "vs"
+    gui.playerChoice["text"] = str(weapons[usersGo])
+    gui.computerChoice["text"] = str(weapons[computersGo])
 
     if (usersGo == computersGo):
         gui.resultLabel["text"] = "A Tie"
@@ -136,6 +94,10 @@ def playGame(gui, usersGo, stats):
     gui.playerScore["text"] = "P " + str(stats.userWon)
     gui.computerScore["text"] = "C " + str(stats.computerWon)
 
+    gui.rockButton["state"] = "normal"
+    gui.paperButton["state"] = "normal"
+    gui.scissorsButton["state"] = "normal"
+
 def main():
     root = tk.Tk()
     gameStats = GameStats()
@@ -143,5 +105,4 @@ def main():
     root.mainloop()
 
 if __name__ == "__main__":
->>>>>>> Keep score of how many games the user and computer have won; update the label to reflect the score
     main()
